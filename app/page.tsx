f0af453d8d2c07c4e9669d436d9e227e073a4b71
Onlook <git@onlook.com>
2025-06-05 03:29:44 +0530

    For this particular subcategory and only this group, add a glassmorphism blurring effect on the image. I want to display the user that this category is "Coming soon".

--- app/page.tsx
+++ app/page.tsx
@@ -142,7 +142,7 @@ export default function Page() {
             title: 'Visual Identity',
             category: 'Design',
             description: 'Creating cohesive and impactful brand experiences',
-            image: 'https://images.unsplash.com/photo-1517842645767-c639042777db?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+            image: 'https://res.cloudinary.com/dexibw60d/image/upload/v1749053305/7_ykmvwr.jpg',
         },
     ];
 
