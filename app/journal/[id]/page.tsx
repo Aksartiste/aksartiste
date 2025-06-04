11805790503cf0bcc5d3c7b11dc3815f82391fc7
Onlook <git@onlook.com>
2025-06-04 17:27:08 +0530

    Update the image:
    https://res.cloudinary.com/dexibw60d/image/upload/v1749038207/output-3_gpz3oa.png

--- app/journal/[id]/page.tsx
+++ app/journal/[id]/page.tsx
@@ -49,7 +49,7 @@ const journalEntries = [
         id: '2',
         title: 'The Art of Authentic Storytelling',
         date: 'May 15, 2023',
-        image: 'https://images.unsplash.com/photo-1671000002?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+        image: 'https://res.cloudinary.com/dexibw60d/image/upload/v1749037809/artcover_ddojin.jpg',
         excerpt:
             'Exploring how vulnerability creates connection in both visual and written narratives.',
         content: `
