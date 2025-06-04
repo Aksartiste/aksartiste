cffdba8ad5cb64a522037fdec82c0433c88733e9
Onlook <git@onlook.com>
2025-06-04 17:30:05 +0530

    I want the full image to display

--- app/journal/[id]/page.tsx
+++ app/journal/[id]/page.tsx
@@ -66,7 +66,7 @@ const journalEntries = [
         id: '3',
         title: 'The Art of Authentic Storytelling',
         date: 'May 15, 2023',
-        image: 'https://images.unsplash.com/photo-1673000003?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+        image: 'https://res.cloudinary.com/dexibw60d/image/upload/v1749038207/output-3_gpz3oa.png',
         excerpt:
             'Exploring how vulnerability creates connection in both visual and written narratives.',
         content: `
