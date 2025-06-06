519eff792ccaae838f7bfcbfbbd8af73ac0970ab
Your Name <you@example.com>
2025-06-06 11:57:06 +0530

    updated next.config for deployment...

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,26 @@
-import path from 'path';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['res.cloudinary.com']
+  },
   output: "standalone",
-  distDir: process.env.NODE_ENV === "production" ? ".next-prod" : ".next",
+  distDir: ".next",
   typescript: {
     ignoreBuildErrors: true
   }
 };
+
 export default nextConfig;
+
+
+
+// import path from 'path';
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "standalone",
+//   distDir: process.env.NODE_ENV === "production" ? ".next-prod" : ".next",
+//   typescript: {
+//     ignoreBuildErrors: true
+//   }
+// };
+// export default nextConfig;
