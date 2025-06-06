91855002b52f2e25f020d5ecbbf3c8421dd4e8b8
Onlook <git@onlook.com>
2025-06-06 13:40:59 +0530

    How can I resolve these errors? If you propose a fix, please make it concise.

--- app/journal/[id]/page.tsx
+++ app/journal/[id]/page.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import Image from 'next/image';
 
 interface JournalEntry {
     id: string;
