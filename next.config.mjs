31d3e6159b5ab14df6f2dd6c0fe4d847795894d1
Onlook <git@onlook.com>
2025-06-06 14:48:11 +0530

    Save before publishing to nnrz6qcbrlr7hcbupp607.onlook.live

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['res.cloudinary.com'],
-        unoptimized: true,
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'res.cloudinary.com',
-                pathname: '/dexibw60d/**',
-            },
-        ],
-    },
-    output: 'standalone',
-    distDir: '.next',
-    typescript: {
-        ignoreBuildErrors: true,
-    },
+  images: {
+    domains: ['res.cloudinary.com'],
+    unoptimized: true,
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'res.cloudinary.com',
+      pathname: '/dexibw60d/**'
+    }]
+  },
+  output: "standalone",
+  distDir: process.env.NODE_ENV === "production" ? ".next-prod" : ".next",
+  typescript: {
+    ignoreBuildErrors: true
+  }
 };
-
 export default nextConfig;
 
 // import path from 'path';
